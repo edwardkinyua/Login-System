92986eef576043906f814380c8f8f32b475d1fdb
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-09-21 21:36:27 -0400

    Updated for test with new login.html

--- curlCall.php
+++ curlCall.php
@@ -1,15 +1,19 @@
 <?php
+        echo 'User:'. $_POST['username']; //Test that data is being passed back and forth
 
-        $middle_url = ""; //Need URL
+/*      Commented out for testing
+        $middle_url = "web.njit.edu/~meu3/index.html";
 
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $middle_url); //Set URL
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //Return response
-//      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_POST, true);
         $result = curl_exec($ch);
         echo $result;
 
         curl_close($ch);
+*/
 ?>
+
