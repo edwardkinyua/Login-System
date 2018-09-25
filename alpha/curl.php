1fb5c63d6e9119cd636c212f858f37d3d76faebd
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-09-24 21:32:48 -0400

    Updated curl.php
    Extra variable printing for testing

--- alpha/curl.php
+++ alpha/curl.php
@@ -2,27 +2,36 @@
         $user = array('username' => $_POST['username'], 'password' =>$_POST['password']);
 
         echo 'Hello, '. $_POST['username'] . ' ' . 'this is a temporary login' . "<br>";
-        echo 'usr: ' . $user['username'] . "<br>";      //Alpha Testing
-        echo 'pwd: ' . $user['password'] . "<br>";      //Alpha Testing
 
+/*      echo 'pw: ' . $_POST['password'] . "<br>";
+        echo 'url:' . $_POST['url'] . "<br>";
+*/
+        echo 'usr: ' . $user['username'] . "<br>";
+        echo 'pwd: ' . $user['password'] . "<br><br>";
 
-/*      $middle_url = "https://jsonplaceholder.typicode.com/todos/1"; //Commented out for testing
+        $middle_url = "https://web.njit.edu/~bkw2/middle.html"; //May change to middle.php
+
 
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $middle_url);     //Set URL
-        curl_setopt($ch, CURLOPT - Ignore
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //Return response
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POST, true);           //
+        curl_setopt($ch, CURLOPT_POST, $user);          //Send array
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //Receive response
 
         $result = curl_exec($ch);
-        var_dump(json_decode($result,true));
-        //$result_decode=json_decode($result), true;
-        //print_r($result);
-        //print_r($result_decode);
-        echo $result;
+
+        if(var_dump(json_decode($result,true)) == NULL)
+                echo ' - VARDUMP HAS NOTHING TO DECODE' . "<br><br>";
+
+        $result_decode=json_decode($result, true) . "<br>";
+
+        echo 'IF RESULTS ARE BLANK, NOTHING RECEIVED' , "<br>";
+        echo 'result: ' . $result . "<br>";
+        echo 'result_decode: ' . $result_decode . "<br>";
 
         curl_close($ch);
-*/
+
 ?>
+
