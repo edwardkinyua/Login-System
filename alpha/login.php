6537ca1fa3bc327f998892881408236f46b17b3a
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-09-27 16:57:54 -0400

    Update login.php

--- alpha/login.php
+++ alpha/login.php
@@ -1,10 +1,8 @@
 <?php
         $user = array('username' => $_POST['username'], 'password' =>$_POST['password']);
 
-        $middle_url = "https://web.njit.edu/~meu3/CS490/test.php";
-//      $middle_url = "https://web.njit.edu/~bkw2/middle.phps";
-//      $middle_url = "https://web.njit.edu/~ek95/backend.phps ";
-//      $middle_url = "https://web.njit.edu/~ek95/test.php ";
+        $middle_url = "https://web.njit.edu/~bkw2/middle.php";
+
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $middle_url);                             //Set URL
@@ -18,13 +16,12 @@
 
         $result = curl_exec($ch);
 
-        $result_decode=json_decode($result, true);
+        $result_decode=json_decode($result);
 
         if($result == false || $result == null) {
                 echo "No result";
         } else {
-                echo 'result: ' . $result . "<br>";
-                echo 'result_decode: ' . $result_decode . "<br>";
+                echo $result . "<br>";
         }
         curl_close($ch);
 
